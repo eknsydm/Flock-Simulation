bb1428e58fcd698c3766706dcb182d785d59ab5c
eknsydm <46608164+eknsydm@users.noreply.github.com>
2024-07-09 16:12:04 +0300

    Basic flock is done

--- scenes/boid.gd
+++ scenes/boid.gd
@@ -3,10 +3,19 @@ extends Area2D
 
 var velocity := Vector2.UP
 var acceleration := Vector2.ZERO
-var max_force := 0.3
-var max_speed := 1
+var max_force := 1
 var boids : Array[Boid]
 var screensize
+
+@export var cohesion_force: = 0.05
+@export var algin_force: = 0.05
+@export var separation_force: = 0.05
+
+@export var avoid_distance :float = 20.0
+@export var view_distance :float = 50.0
+
+@export var max_speed :float = 8
+
 
 func _ready() -> void:
 	velocity = velocity.rotated(PI * randi())
@@ -14,10 +23,11 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
+	var acceleration = get_acceleration()
+	
+	velocity = (velocity + acceleration).limit_length(max_speed)
 	position += velocity
-	velocity += acceleration
-	rotation = lerp_angle(rotation, Vector2.ZERO.angle_to_point(velocity), 0.8)
-	flock()
+	#rotation = lerp_angle(rotation, Vector2.ZERO.angle_to_point(velocity), 0.8)
 	edges()
 
 func edges():
@@ -30,27 +40,46 @@ func edges():
 	if global_position.y > screensize.y:
 		global_position.y = 0
 
-func align() -> Vector2:
-	var steering = Vector2.ZERO
-	for boid:Boid in boids:
-		steering += boid.velocity
+func get_flock_status(flock: Array[Boid]) -> Array[Vector2]:
+	var center_vector:Vector2
+	var flock_center:Vector2
+	var align_vector: Vector2
+	var avoid_vector: Vector2
 	
-	if boids.size() > 0:
-		steering /= boids.size()
-		steering -= velocity
-		if steering.length() != 0:
-			steering = Vector2(
-				steering.x * max_speed / steering.length(),
-				steering.y * max_speed / steering.length())
-		steering = steering.limit_length(max_force)
-	return steering
+	for f in flock:
+		var neighbor_pos: Vector2 = f.global_position
 
-func flock():
-	var allignment = align()
-	acceleration = allignment
+		align_vector += f.velocity
+		flock_center += neighbor_pos
+
+		var d = global_position.distance_to(neighbor_pos)
+		#!
+		if d > 0 and d < avoid_distance:
+			avoid_vector -= (neighbor_pos - global_position).normalized() * (avoid_distance / d * max_speed)
+		#!
+
+	var flock_size = flock.size()
+	if flock_size:
+		align_vector /= flock_size
+		flock_center /= flock_size
+
+		var center_dir = global_position.direction_to(flock_center)
+		var center_speed = (max_speed * 
+			(global_position.distance_to(flock_center) / 
+			view_distance))
+		center_vector = center_dir * center_speed
+
+	return [center_vector, align_vector, avoid_vector]
+
+
+func get_acceleration():
+	var flock_status = get_flock_status(boids)
 	
-func _init() -> void:
-	pass
+	var cohesion_vector = flock_status[0] * cohesion_force
+	var align_vector = flock_status[1] * algin_force
+	var separation_vector = flock_status[2] * separation_force
+
+	return cohesion_vector + align_vector + separation_vector #+ mouse_vector
 
 
 func _on_vision_area_entered(area: Area2D) -> void:
